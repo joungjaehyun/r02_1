205e7925a2c5ca9ae0a5653c3fc46f27798a7f78
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-14 14:31:35 +0900

    Update LoginComponent.js

--- src/components/member/LoginComponent.js
+++ src/components/member/LoginComponent.js
@@ -3,34 +3,42 @@ import { useDispatch } from "react-redux";
 import { requestLogin } from "../../reducers/loginSlice";
 
 const initState = {
-    email:'user00@aaa.com',
-    pw:'1111'
+    email: 'user00@aaa.com',
+    pw: '1111'
 }
 
 const LoginComponenet = () => {
-    
-    const [loginInfo, setLoginInfo] =useState({...initState})
-    
+
+    const [loginInfo, setLoginInfo] = useState({ ...initState })
+
     const dispatch = useDispatch()
 
-    return (  
-            <div className="m-3 p-3">
+    return (
+        <div className="m-3 p-3  flex justify-center">
+            <div>
+                <div className="m-2 p-2 border-4 border-orange-300/75">
+                    <div>
+                        <span>ID Login</span>
+                    </div>
+                    <div className="m-2 p-2">
+                        
+                        <input className="bg-black" type="text" name="email" value={loginInfo.email} onChange={() => { }}></input>
+                    </div>
+                    <div className="m-2 p-2">
+
+                        <input className="bg-black" type="password" name="pw" value={loginInfo.pw} onChange={() => { }}></input>
+                    </div>
+                
                 <div>
-                    <label>Email - </label>
-                    <input className="bg-black" type="text" name="email" value={loginInfo.email} onChange={()=>{}}></input>
+                    <button className="border-2 m-2  mb-10" onClick={() => dispatch(requestLogin(loginInfo))}>LOGIN</button>
                 </div>
-                <div className="m-3 p-3">
-                    <label>Password - </label>
-                    <input className="bg-black" type="password" name="pw" value={loginInfo.pw} onChange={()=>{}}></input>
-                </div>
-                <div>
-                    <button onClick={()=> dispatch(requestLogin(loginInfo))}>LOGIN</button>
                 </div>
                 <div className="flex justify-center items-center">
-                <img src={require('../../image/backimage.jpg')} ></img>
+                    <img src={require('../../image/backimage.jpg')} ></img>
                 </div>
-            </div>    
+            </div>
+        </div>
     );
 }
- 
+
 export default LoginComponenet;
